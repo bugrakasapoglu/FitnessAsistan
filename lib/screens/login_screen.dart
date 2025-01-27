aa85c4e75f9cd0a6d30e2f42b7ceb9ca18f1c3ad
Buğra Kasapoğlu <kasapoglubugra75@gmail.com>
2025-01-27 14:17:57 +0300

    Giriş Ekranı Düzenlendi.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,8 +12,44 @@ class _LoginScreenState extends State<LoginScreen> {
    Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text ("Giris Ekranım"),
+      body:  Center(
+        child: SizedBox(
+          width: 250,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Column(
+                children: [
+                  const Center(
+                    child: Text(""),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                    hintText: "E-Posta",
+                   // helper: Text("E-Postanızı Giriniz"),
+                    border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                    hintText: "Şifre",
+                   // helper: Text("Şifrenizi Giriniz"),
+                    border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(onPressed: () {}, child: const Text("Giriş Yap"),
+                  ),
+                
+                ],
+                ),
+              ),
+            ],
+          ) 
+        ),
       ), // Center
     ); // Scaffold
    }
